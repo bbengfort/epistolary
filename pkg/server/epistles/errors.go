77349d0ad71a6fadfd7882976bbe170d2a0f310e
Benjamin Bengfort <benjamin@bengfort.com>
2023-01-13 14:48:07 -0600

    Fetch when Creating Epistle (#3)

--- pkg/server/epistles/errors.go
+++ pkg/server/epistles/errors.go
@@ -3,5 +3,6 @@ package epistles
 import "errors"
 
 var (
-	ErrIDRequired = errors.New("cannot execute query without an id stored on the model")
+	ErrIDRequired   = errors.New("cannot execute query without an id stored on the model")
+	ErrLinkRequired = errors.New("cannot fetch epistle information without a link")
 )
